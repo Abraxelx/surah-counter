d507f4cc9ac2c9d5923e7c1ce2de76735cf55aeb
sinankogce <kogceweb@gmail.com>
2023-03-14 23:03:39 +0300

    surah count update and get added

---
+++ src/main/java/com/digiduty/qurancounter/form/SurahForm.java
@@ -0,0 +1,9 @@
+package com.digiduty.qurancounter.form;
+
+import lombok.Data;
+
+@Data
+public class SurahForm {
+    String surahType;
+    int count;
+}
--- src/main/java/com/digiduty/qurancounter/mvc/controller/HomeController.java
+++ src/main/java/com/digiduty/qurancounter/mvc/controller/HomeController.java
@@ -1,13 +1,51 @@
 package com.digiduty.qurancounter.mvc.controller;
 
+import com.digiduty.qurancounter.form.SurahForm;
+import com.digiduty.qurancounter.model.SurahsEnum;
+import com.digiduty.qurancounter.service.CountService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.concurrent.ExecutionException;
 
 @Controller
 public class HomeController {
 
+    final CountService countService;
+
+    public HomeController(CountService countService) {
+        this.countService = countService;
+    }
+
+
     @GetMapping("/")
-    public String home1() {
+    public String home1(ModelMap modelMap) {
+
+        try {
+            modelMap.addAttribute("surahForm",new SurahForm());
+            modelMap.addAttribute("allCounts",countService.getAllCounts());
+        } catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return "index";
+    }
+
+    @PostMapping("/updateSurahCount")
+    public String updateSurahs(@ModelAttribute(value = "surahForm") SurahForm surahForm, ModelMap modelMap) {
+
+        try {
+            countService.updateCounts(SurahsEnum.of(surahForm.getSurahType()), surahForm.getCount());
+            modelMap.addAttribute("surahForm",new SurahForm());
+            modelMap.addAttribute("allCounts",countService.getAllCounts());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         return "index";
     }
 
