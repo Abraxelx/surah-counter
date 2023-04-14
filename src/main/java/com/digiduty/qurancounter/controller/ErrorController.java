67e219b0733b8e27eecd5e314428d6b03df4e334
Halil Sahin <xelilim@hotmail.com>
2023-04-14 21:51:13 +0300

    Added Metatags , added 404 Error Page

---
+++ src/main/java/com/digiduty/qurancounter/controller/ErrorController.java
@@ -0,0 +1,14 @@
+package com.digiduty.qurancounter.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
+    private static final String PATH = "/error";
+
+    @RequestMapping(value = PATH)
+    public String error() {
+        return "404";
+    }
+}
